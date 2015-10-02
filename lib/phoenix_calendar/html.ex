1a6dd8ebb2a76818c67fc9cdaf579746d31862b2
Lau Taarnskov <lau@taarnskov.com>
2015-10-02 14:37:32 -0500

    Add support for Calecto structs.

--- lib/phoenix_calendar/html.ex
+++ lib/phoenix_calendar/html.ex
@@ -1,5 +1,5 @@
 if Code.ensure_loaded?(Phoenix.HTML) do
-  defimpl Phoenix.HTML.Safe, for: [Calendar.DateTime] do
+  defimpl Phoenix.HTML.Safe, for: [Calendar.DateTime, Calecto.DateTime, Calecto.DateTimeUTC] do
     def to_iodata(%{usec: usec} = t) when usec == nil do
       "#{Calendar.Strftime.strftime!(t, "%F %T")} #{t.timezone}"
     end
@@ -8,21 +8,21 @@ if Code.ensure_loaded?(Phoenix.HTML) do
       "#{Calendar.Strftime.strftime!(t, "%F %T")}.#{decimals} #{t.timezone}"
     end
   end
-  defimpl Phoenix.HTML.Safe, for: [Calendar.NaiveDateTime] do
+  defimpl Phoenix.HTML.Safe, for: [Calendar.NaiveDateTime, Calecto.NaiveDateTime] do
     def to_iodata(%{usec: usec} = t) when usec == nil do
-      "#{Calendar.Strftime.strftime!(t, "%F %T")} #{t.timezone}"
+      "#{Calendar.Strftime.strftime!(t, "%F %T")}"
     end
     def to_iodata(t) do
       decimals = String.rjust("#{t.usec}", 6, ?0) # pad microsecs with zeroes
-      "#{Calendar.Strftime.strftime!(t, "%F %T")}.#{decimals} #{t.timezone}"
+      "#{Calendar.Strftime.strftime!(t, "%F %T")}.#{decimals}"
     end
   end
-  defimpl Phoenix.HTML.Safe, for: [Calendar.Date] do
+  defimpl Phoenix.HTML.Safe, for: [Calendar.Date, Calecto.Date] do
     def to_iodata(t) do
       Calendar.Strftime.strftime!(t, "%F")
     end
   end
-  defimpl Phoenix.HTML.Safe, for: [Calendar.Time] do
+  defimpl Phoenix.HTML.Safe, for: [Calendar.Time, Calecto.Time] do
     def to_iodata(t) do
       Calendar.Strftime.strftime!(t, "%T")
     end
