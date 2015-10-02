b40f134144c5a0026cb00d41c938b77c31ee77ff
Lau Taarnskov <lau@taarnskov.com>
2015-10-02 14:33:50 -0500

    Print usec decimals for datetimes if available.

--- lib/phoenix_calendar/html.ex
+++ lib/phoenix_calendar/html.ex
@@ -1,12 +1,20 @@
 if Code.ensure_loaded?(Phoenix.HTML) do
   defimpl Phoenix.HTML.Safe, for: [Calendar.DateTime] do
-    def to_iodata(t) do
+    def to_iodata(%{usec: usec} = t) when usec == nil do
       "#{Calendar.Strftime.strftime!(t, "%F %T")} #{t.timezone}"
+    end
+    def to_iodata(t) do
+      decimals = String.rjust("#{t.usec}", 6, ?0) # pad microsecs with zeroes
+      "#{Calendar.Strftime.strftime!(t, "%F %T")}.#{decimals} #{t.timezone}"
     end
   end
   defimpl Phoenix.HTML.Safe, for: [Calendar.NaiveDateTime] do
+    def to_iodata(%{usec: usec} = t) when usec == nil do
+      "#{Calendar.Strftime.strftime!(t, "%F %T")} #{t.timezone}"
+    end
     def to_iodata(t) do
-      Calendar.Strftime.strftime!(t, "%F %T")
+      decimals = String.rjust("#{t.usec}", 6, ?0) # pad microsecs with zeroes
+      "#{Calendar.Strftime.strftime!(t, "%F %T")}.#{decimals} #{t.timezone}"
     end
   end
   defimpl Phoenix.HTML.Safe, for: [Calendar.Date] do
